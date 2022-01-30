3a5320cc57279dca052f322f45b62b297ed6e62b
daisuketakeo <daisuke_takeo@kp3.co.jp>
2022-01-30 22:56:37 +0900

    TESTアプリ名変更

---
+++ src/test/java/com/cpc/CpcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cpc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CpcDsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
