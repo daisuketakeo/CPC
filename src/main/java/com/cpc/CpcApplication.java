52381033b81f254790f252051587ec00b0827e8a
daisuketakeo <daisuke_takeo@kp3.co.jp>
2022-01-30 22:25:44 +0900

    モジュール名修正

---
+++ src/main/java/com/cpc/CpcApplication.java
@@ -0,0 +1,13 @@
+package com.cpc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CpcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CpcApplication.class, args);
+	}
+
+}
